a6ef83ba87cab4dc187541eaca8de85cd594b706
zorikg <37661625+zorikg@users.noreply.github.com>
2020-06-02 19:13:01 +0300

    Update eval_main.py

--- code/eval_main.py
+++ code/eval_main.py
@@ -4,6 +4,9 @@
 import sys
 
 def main(base_path):
+    # Receives path to root dir, containing the annotated data and DA + systems scores from WMT19.
+    # Calculates KoBE for all system outputs, caculcates Pearson correlation to human DA and 
+    # compares it to the submitted systems.
     
 
 if __name__ == '__main__':
